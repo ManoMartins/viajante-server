1949ab35238a9e1a1862ed8f16c0899e68efd3d7
Manoel Martins <mano.martins29@gmail.com>
2022-02-17 00:24:18 -0300

    feat(main): config upload file and static

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { graphqlUploadExpress } from 'graphql-upload';
+import { join } from 'node:path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
+  app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/photos' });
   await app.listen(3333);
 }
 bootstrap();
