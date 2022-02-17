d2c35c4dc7c0e964062a4711d1a30706efe73639
Manoel Martins <mano.martins29@gmail.com>
2022-02-17 01:17:30 -0300

    refactor(app): remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoryModule } from './category/category.module';
 import { CitiesModule } from './cities/cities.module';
 
 @Module({
@@ -15,10 +12,7 @@ import { CitiesModule } from './cities/cities.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     TypeOrmModule.forRoot(),
-    CategoryModule,
     CitiesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
